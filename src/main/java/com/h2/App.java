9732fdbe98fa4f85672834cbec2c5fe990265ba2
Ranjay Salmon <ranjy23@gmail.com>
2022-07-23 00:13:36 -0400

    Chapter 2

--- src/main/java/com/h2/App.java
+++ src/main/java/com/h2/App.java
@@ -1,17 +1,26 @@
 package com.h2;
 
+import java.util.Arrays;
+
 /**
  * Hello world!
- *
  */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+public class App {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
     }
 
     public static int doubleTheNumber(int number) {
-        return -1;
+        return number * 2;
     }
+
+    private static int add(int[] numbers) {
+        return Arrays.stream(numbers).reduce(0, Integer::sum);
+//        int sum = 0;
+//        for(int num : numbers){sum += num;}
+//        return sum;
+    }
+
+
+
 }
